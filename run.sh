fb078d005ebae8f2331de0e2dc3be3c10aabe4e4
Alice Monday <alice0meta@gmail.com>
2014-07-23 22:58:58 -0700

    tweak

--- run.sh
+++ run.sh
@@ -8,4 +8,5 @@ if [ ! -f installed ]; then
 	echo "tagtime has been installed" > installed
 fi
 
+./stop.sh
 forever start -a -l forever.txt -o ~/.forever/tt_out.txt -e ~/.forever/tt_err.txt --minUptime 1000ms --spinSleepTime 20s tagtime.js $@
