a1777c0ade363ab7bf9431778639635f3cc5b8d4
Alice Monday <alice0meta@gmail.com>
2014-07-27 01:00:26 -0700

    ???

--- tagtime.js
+++ tagtime.js
@@ -413,7 +413,7 @@ module.exports.main = function(args){
 	var argv = args.argv
 	switch (argv[0]) {
 		case undefined: run_pings(); break
-		case 'prompt' : prompt({time:i(argv[1]), last_doing:argv[2]},function(e,tags){print(tags); process.exit()}); break
+		case 'prompt' : prompt({time:i(argv[1]), last_doing:argv[2]},function(e,tags){print(tags);}); break
 		case 'sync'   : tt_sync(); process.exit(); break
 		case 'merge'  : merge(argv[1]); break
 		case 'e'      : print(eval(argv.slice(1).join(' '))); break
